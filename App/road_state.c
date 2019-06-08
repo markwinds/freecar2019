0d486cf5f985b620548c9ae5383a567a47bd9d81
markwinds <38316819+markwinds@users.noreply.github.com>
2019-06-08 20:21:11 +0800

    代码整理好了

--- App/road_state.c
+++ App/road_state.c
@@ -3,20 +3,22 @@
 
 /**状态保护位*/
 uint8 protect_flag = 0;
-uint32 timer_flag = 100;
 
-void checkSmallCircleLeft();
 
 /******************************************定时器操作************************************/
+/*
+ *  @brief      开启PIT0定时器和中断
+ *  @param      time_long       定时的时长
+ *  @param      deal_function   中断处理函数
+ */
 void setRoadTypeTimer(int16 time_long, void (*deal_function)())
 {
     if (time_long <= 0)
     {
         return;
     }
-    pit_init_ms(PIT0, time_long);
     set_vector_handler(PIT0_VECTORn, deal_function);
-    enable_irq(PIT0_IRQn);
+    pit_init_ms(PIT0, time_long);
 }
 
 void resetTimer()
@@ -25,33 +27,31 @@ void resetTimer()
     protect_flag = 0;
 }
 
-/******************************************函数************************************/
-
+/***************************************相关函数************************************/
+/*
+ *  @brief      初始化道路判断的定时器中断
+ */
 void initJudgeRoad()
 {
     pit_close(PIT0);
     enable_irq(PIT0_IRQn);
 }
 
+/*
+ *  @brief      更新道路状态
+ *  @param      new_road_state       新的路况
+ */
 void updataRoadState(RoadState *new_road_state)
 {
+    /**更新当前状态*/
     last_road_state = now_road_state;
     now_road_state->out_state();
     now_road_state = new_road_state;
     /**开启状态保护*/
     protect_flag = new_road_state->protect_time <= 0 ? 0 : 1;
+    /**进入状态的预备操作*/
     now_road_state->enter_state();
-    //setRoadTypeTimer(now_road_state->protect_time, now_road_state->deal_timer);
-    if (now_road_state->protect_time <= 0)
-    {
-        return;
-    }
-    pit_init_ms(PIT0, now_road_state->protect_time);
-    set_vector_handler(PIT0_VECTORn, checkSmallCircleLeft);
-    getFlag();
-    //getFlag();
-    enable_irq(PIT0_IRQn);
-    //getFlag();
+    setRoadTypeTimer(now_road_state->protect_time, now_road_state->deal_timer);
 }
 
 /*********************************************doNothing************************************/
@@ -59,49 +59,36 @@ void doNothings()
 {
 }
 
-/****************************************small_circle_left************************************/
+/****************************************enter function************************************/
+/*
+ *  @brief      用蜂鸣器发出该路况对应的声音
+ */
 void makeSound()
 {
-    // if (last_road_state == &small_circle_left)
-    // {
-    //     setSteer(-50);
-    //     DELAY_MS(2000);
-    // }
-    //if (fuck_you < 1)
     tellMeRoadType(now_road_state->sound_type);
-    printf("buzzer\n");
 }
-void justTurnLED()
-{
-    resetTimer();
-    led_turn(LED1);
-}
+
+/****************************************interrupt function************************************/
 
 void checkSmallCircleLeft()
 {
-
     resetTimer();
     int32 mid = getADCVaule(horizontal_middle);
     int32 left = getADCVaule(horizontal_left);
-    //int32 right = getADCVaule(horizontal_right);
     if (mid + left > 1800)
     {
-        fuck_you++;
         setSteer(-50);
-        //printf("turn\n");
-        DELAY_MS(2000);
-        led_turn(LED3);
+        DELAY_MS(200);
     }
-    //resetTimer();
 }
 
 /*********************************************定义道路类型************************************/
-RoadState straighta_way = {0, doNothings, doNothings, T2L1L1, doNothings};
-// RoadState big_circle_left = {0, doNothings, doNothings, T2L1L1, doNothings};
-// RoadState big_circle_right = {0, doNothings, doNothings, T2L1L1, doNothings};
-RoadState small_circle_left = {0, makeSound, doNothings, T1L5, justTurnLED};
-RoadState small_circle_left1 = {3000, makeSound, doNothings, T1L5, checkSmallCircleLeft};
-// RoadState small_circle_right = {0, doNothings, doNothings, T1L5, doNothings};
+RoadState straighta_way = {0, doNothings, doNothings, T2L1L1, resetTimer};
+// RoadState big_circle_left = {0, doNothings, doNothings, T2L1L1, resetTimer};
+// RoadState big_circle_right = {0, doNothings, doNothings, T2L1L1, resetTimer};
+RoadState small_circle_left_in = {500, makeSound, doNothings, T1L5, checkSmallCircleLeft};
+//RoadState small_circle_left1 = {3000, makeSound, doNothings, T1L5, checkSmallCircleLeft};
+// RoadState small_circle_right = {0, doNothings, doNothings, T1L5, resetTimer};
 
 RoadState *now_road_state = &straighta_way;
 RoadState *last_road_state = &straighta_way;
@@ -115,7 +102,7 @@ void judgeRoadFromADC(int32 mid, int32 left, int32 right)
     }
     if (mid + left > 1900)
     {
-        updataRoadState(&small_circle_left1);
+        updataRoadState(&small_circle_left_in);
     }
     else if (mid + right > 1900)
     {
